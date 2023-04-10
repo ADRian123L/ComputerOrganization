e2d44355f5850947917bbe304b7c0f270e304263
ADRian123L <adrian163@usf.edu>
2023-04-10 12:02:01 -0400

    Added comments in pro. 4 assign 7

--- Computer Organization/Assignments/Assignment7/CODE/problem4.s
+++ Computer Organization/Assignments/Assignment7/CODE/problem4.s
@@ -2,8 +2,8 @@
 
 .data
     # Array of integers:
-    arr: .word 64, 25, 12, 22, 11, 32, 1, 3, 4, 5, 6
-    len: .word 11
+    arr: .word 64, 25, 12, 22, 11, 32, 1, 3, 4, 5, 6, 7, 1, 0, 56
+    len: .word 15
 
 .text
 main:
@@ -35,7 +35,7 @@ main:
                 add t0, x0, x0
                 for_print:
                     # Condition:
-                    bge t0, t5, end_loop_print
+                    bge t0, t5, end_loop_print # t0 (counter) >= t5 goto end_loop_print
                     # Body:
                     slli t1, t0, 2
                     add t1, t4, t1
@@ -89,29 +89,31 @@ main:
                         add s5, a1, x0
             
                         # Sort the array:
-                        addi s6, a1, -1 # n - 1 
+                        addi s6, a1, -1 # pass n - 1 
                         add s0, x0, x0 # i = 0
                         for:
-                            bge s0, s6, end_loop # i > n
+                            bge s0, s6, end_loop # i > n goto end_loop
 
                             # Call findMinimum function:
-                            slli t0, s0, 2
-                            add a0, s4, t0
-                            sub a1, s5, s0
+                            slli t0, s0, 2  
+                            add a0, s4, t0 # pass &array[i]
+                            sub a1, s5, s0 # pass n - i 
                             jal x1, findMinimum
+                            
+                            
 
                             # Set min_idx to the value in a0:
-                            add s2, a0, x0
+                            add s2, a0, x0 
 
                             if_swap:
-                                beq s2, s0, continue
+                                beq s2, s0, continue # min_idx == i
                                 # Call the swap function:
-                                add t0, s2, s0
-                                slli t0, t0, 2
-                                add a0, s4, t0
-                                slli t0, s0, 2
-                                add a1, s4, t0
-                                jal x1, swap
+                                add t0, s2, s0 # t0 = min_idx + i
+                                slli t0, t0, 2 
+                                add a0, s4, t0 # a0 = &array[min_idx + i]
+                                slli t0, s0, 2 
+                                add a1, s4, t0 # a1 = &array[i]
+                                jal x1, swap # Call swap
 
                             continue:
 
@@ -119,7 +121,7 @@ main:
                             addi s0, s0, 1
 
                             # Jump to Loop:
-                            jal x0, for 
+                            j for
 
                         end_loop:
 
@@ -146,27 +148,28 @@ main:
                     # t1 is min_E
 
                     # Initialize min_idx and min_E:
-                    add t0, x0, x0
-                    add t2, a0, t0
-                    lw t1, 0(t2)
+                    add t0, x0, x0 # min_idx = 0
+                    add t2, a0, t0 # a0 + min_idx
+                    lw t1, 0(t2) # t1 = array[a0 + min_idx]
 
                     # Loop through the array:
                     # t3 is i
                     addi t3, x0, 1
                     for_loop_2:
                         # Condition:
-                        bge t3, a1, end_loop_2
+                        bge t3, a1, end_loop_2 # i > a0 goto end_loop_2
                         # Body:
                         slli t4, t3, 2
-                        add t4, a0, t4
+                        add t4, a0, t4 # t4 = &array[i]
+                        
                         # t5 = arr[i]
                         lw t5, 0(t4)
                         if_2:
-                            bge t5, t1, continue_2
+                            bge t5, t1, continue_2  # array[i] >= arrar[a0 + min_idx]  
                             add t0, t3, x0
                             slli t6, t0, 2
                             add t6, a0, t6
-                            lw t1, 0(t6)
+                            lw t1, 0(t6) # t1 == array[i]
 
                         continue_2:
                         
@@ -190,7 +193,7 @@ main:
                 # Interchange the values:
                 lw t0, 0(a0) # temp = a0
                 lw t1, 0(a1) # a0 = a1 
-                sw t1, 0(a0)
+                sw t1, 0(a0) 
                 sw t0, 0(a1)
 	
                 # Return:
